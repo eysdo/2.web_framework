5886717895926e48ed4397aed4d0f713b808b6fc
eysdo <308418897@qq.com>
2022-06-20 11:04:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 #导入setup函数并传参
 setup(
     name = 'EWeb',  #名字
-    version = 'v17',    #版本
+    version = 'v1.0',    #版本
     description = 'Python WSGI WEB Framework',  #描述信息
     author = 'eysdo',   #作者
     author_email='308418897@qq.com',    #作者邮箱地址
@@ -27,21 +27,21 @@ setup(
     warning: sdist: standard file 'setup.py' not found
 
     writing manifest file 'MANIFEST'
-    creating EWeb-v17
-    creating EWeb-v17\eysdoweb
-    making hard links in EWeb-v17...
-    hard linking eysdoweb\__init__.py -> EWeb-v17\eysdoweb
-    hard linking eysdoweb\app.py -> EWeb-v17\eysdoweb
-    hard linking eysdoweb\setup.py -> EWeb-v17\eysdoweb
-    hard linking eysdoweb\web.py -> EWeb-v17\eysdoweb
+    creating EWeb-v1.0
+    creating EWeb-v1.0\eysdoweb
+    making hard links in EWeb-v1.0...
+    hard linking eysdoweb\__init__.py -> EWeb-v1.0\eysdoweb
+    hard linking eysdoweb\app.py -> EWeb-v1.0\eysdoweb
+    hard linking eysdoweb\setup.py -> EWeb-v1.0\eysdoweb
+    hard linking eysdoweb\web.py -> EWeb-v1.0\eysdoweb
     creating dist
     Creating tar archive
-    removing 'EWeb-v17' (and everything under it)
-    pip install .\dist\EWeb-v17.tar.gz    #安装到D:\ProgramData\Anaconda3\Lib\site-packages\eysdoweb
+    removing 'EWeb-v1.0' (and everything under it)
+    pip install .\dist\EWeb-v1.0.tar.gz    #安装到D:\ProgramData\Anaconda3\Lib\site-packages\eysdoweb
     Building wheels for collected packages: EWeb
     Building wheel for EWeb (setup.py) ... done
     Stored in directory: C:\Users\eysdo\AppData\Local\pip\Cache\wheels\51\5a\89\cb2de63db7677f30a01d7ff4ee58b3a7e476b9a0b79bed8413
     Successfully built EWeb
     Installing collected packages: EWeb
-    Successfully installed EWeb-17
+    Successfully installed EWeb-1.0
 '''
